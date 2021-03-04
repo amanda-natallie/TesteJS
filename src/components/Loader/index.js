b314dafd9419ea16095806442cb6e6ef932c77c1
Amanda Natallie <44005675+amanda-natallie@users.noreply.github.com>
2021-03-04 16:18:39 -0300

    fix className on loader and body height

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
 
-const Loader = ({ timer = "" }) => <div class="loader">{timer}</div>;
+const Loader = ({ timer = "" }) => <div className="loader">{timer}</div>;
 
 export default Loader;
